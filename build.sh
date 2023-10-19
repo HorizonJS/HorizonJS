98ba7e9a3ea74733b477fdb70a0dbfb21e3e8d07
jaxdad <you@example.com>
2023-10-19 21:26:56 +0000

    cicd

--- build.sh
+++ build.sh
@@ -7,42 +7,53 @@ tag="$1"
 echo "Deleting old build..."
 rm -Rf ./build
 
+mkdir -p ./build/build/
+mkdir -p ./build/docs/
+mkdir -p ./build/examples/
+
 echo "Building JSBuild and running on source..."
 rm -Rf ./jsbuild/obj
 rm -Rf ./jsbuild/bin
 docker rm horizonjs-jsbuild
 docker build -t horizonjs/jsbuild:latest -f ./Dockerfile-jsbuild .
 docker create --name=horizonjs-jsbuild horizonjs/jsbuild:latest
-docker cp horizonjs-jsbuild:/build ./
-echo '!!!!!!!'
-ls -la ./build
-echo '!!!!!!!'
+docker cp horizonjs-jsbuild:/build/. ./build/build/
+# echo '!!!!!!!'
+# ls -la ./build
+# echo '!!!!!!!'
 
 echo "Building Extdoc and running on source..."
-rm -Rf ./build/docs
 docker rm horizonjs-extdoc
 docker build -t horizonjs/extdoc:latest -f ./Dockerfile-extdoc .
 docker create --name=horizonjs-extdoc horizonjs/extdoc:latest
-docker cp horizonjs-extdoc:/docs/ ./build
+docker cp horizonjs-extdoc:/docs/. ./build/docs/
 
 echo "Copying examples..."
-cp -Rf ./extjs-2.0.2/examples ./build/examples
+cp -Rf ./extjs-2.0.2/examples/. ./build/examples/
 
-echo "Deleting unused paths..."
-rm -Rf ./build/source
-rm -Rf ./build/build
-rm -Rf ./build/package
-rm -Rf ./build/resources/raw-images
-find ./build/resources/css -type f -not -name 'ext-all.css' -not -name 'xtheme-gray.css' -delete
-rm -Rf ./build/resources/license.txt
-rm -Rf ./build/resources/resources.jsb
+# echo "Deleting unused paths..."
+rm -Rf ./build/build/source
+rm -Rf ./build/build/build/
+rm -Rf ./build/build/package
+rm -Rf ./build/build/resources/raw-images
+find ./build/build/resources/css -type f -not -name 'ext-all.css' -not -name 'xtheme-gray.css' -delete
+rm -Rf ./build/build/resources/license.txt
+rm -Rf ./build/build/resources/resources.jsb
 
 echo "Copying overlay..."
-cp -Rf ./overlay/* ./build/
+cp -Rf ./overlay/build/. ./build/build/
+cp -Rf ./overlay/docs/. ./build/docs/
+cp -Rf ./overlay/examples/. ./build/examples/
 
 echo "Compressing..."
-pushd ./build
-zip -q -r "../build/horizonjs-$tag.zip" .
+pushd ./build/build/
+zip -q -r "../horizonjs-$tag.zip" *
+popd
+pushd ./build/docs/
+zip -q -r "../horizonjs-docs-$tag.zip" *
+popd
+pushd ./build/examples/
+zip -q -r "../horizonjs-examples-$tag.zip" *
 popd
 
 echo "Done!"
